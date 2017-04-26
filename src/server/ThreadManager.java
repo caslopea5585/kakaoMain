ebc7238bd996aaec0890f3653c91b9b570d6e3b2
user\user1 <yukyung1994@gmail.com>
2017-04-26 19:49:53 +0900

    no message

--- src/server/ThreadManager.java
+++ src/server/ThreadManager.java
@@ -5,30 +5,21 @@ import java.util.Vector;
 
 
 public class ThreadManager {
-	Server_chat sever_chat;
-	Socket socket;
-	String id;
-	
-	Vector<ThreadManager> userThread=new Vector<ThreadManager>();
-	
-	public ThreadManager(Socket socket,Vector<ThreadManager> userThread,int index) {
-		this.socket=socket;
-		this.userThread=userThread;
-<<<<<<< HEAD
+   Server_chat sever_chat;
+   Socket socket;
+   String id;
+   
+   Vector<ThreadManager> userThread=new Vector<ThreadManager>();
+   
+   public ThreadManager(Socket socket,Vector<ThreadManager> userThread,int index) {
+      this.socket=socket;
+      this.userThread=userThread;
+      sever_chat=new Server_chat(socket,userThread,index);
+      
 
-		sever_chat=new Server_chat(socket,userThread);
-
-		//chat=new Server_chat(socket,userThread);
-
-
-=======
-		sever_chat=new Server_chat(socket,userThread,index);
-		
->>>>>>> 497950331bdb0bac82aecb9f61f1deb3fdc3f317
-
-	}
-	
-	public void start(){
-		sever_chat.start();
-	}
+   }
+   
+   public void start(){
+      sever_chat.start();
+   }
 }
